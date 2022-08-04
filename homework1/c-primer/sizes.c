43b88d66f5e255944370fbd56a1e8b898e06f71c
Eduard Arabian <eduard.arabian@gmail.com>
2022-08-04 11:21:03 +0300

    sizes

--- homework1/c-primer/sizes.c
+++ homework1/c-primer/sizes.c
@@ -7,56 +7,39 @@
 #define PRINT_SIZE(A) (printf("size of "#A" : %zu bytes \n", sizeof(A)))
 
 int main() {
-  // Please print the sizes of the following types:
-  // int, short, long, char, float, double, unsigned int, long long
-  // uint8_t, uint16_t, uint32_t, and uint64_t, uint_fast8_t,
-  // uint_fast16_t, uintmax_t, intmax_t, __int128, and student
+    PRINT_SIZE(int);
+    PRINT_SIZE(short);
+    PRINT_SIZE(long);
+    PRINT_SIZE(char);
+    PRINT_SIZE(float);
+    PRINT_SIZE(double);
+    PRINT_SIZE(unsigned int);
+    PRINT_SIZE(long long);
+    PRINT_SIZE(uint8_t);
+    PRINT_SIZE(uint16_t);
+    PRINT_SIZE(uint32_t);
+    PRINT_SIZE(uint64_t);
+    PRINT_SIZE(uint_fast8_t);
+    PRINT_SIZE(uint_fast16_t);
+    PRINT_SIZE(uintmax_t);
+    PRINT_SIZE(intmax_t);
+    PRINT_SIZE(__int128);
 
-  // Here's how to show the size of one type. See if you can define a macro
-  // to avoid copy pasting this code.
-  PRINT_SIZE(int);
-  PRINT_SIZE(short);
-  PRINT_SIZE(long);
-  PRINT_SIZE(char);
-  PRINT_SIZE(float);
-  PRINT_SIZE(double);
-  PRINT_SIZE(unsigned int);
-  PRINT_SIZE(long long);
-  PRINT_SIZE(uint8_t);
-  PRINT_SIZE(uint16_t);
-  PRINT_SIZE(uint32_t);
-  PRINT_SIZE(uint64_t);
-  PRINT_SIZE(uint_fast8_t);
-  PRINT_SIZE(uint_fast16_t);
-  PRINT_SIZE(uintmax_t);
-  PRINT_SIZE(intmax_t);
-  PRINT_SIZE(__int128);
+    typedef struct {
+        int id;
+        int year;
+    } student;
 
-  //      PRINT_SIZE("int", int);
-  //      PRINT_SIZE("short", short);
+    student you;
+    you.id = 12345;
+    you.year = 4;
 
-  // Alternatively, you can use stringification
-  // (https://gcc.gnu.org/onlinedocs/cpp/Stringification.html) so that
-  // you can write
-  // e.g. PRINT_SIZE(int);
-  //      PRINT_SIZE(short);
+    PRINT_SIZE(you);
 
-  // Composite types have sizes too.
-  typedef struct {
-    int id;
-    int year;
-  } student;
-
-  student you;
-  you.id = 12345;
-  you.year = 4;
+    int x[5];
+    PRINT_SIZE(x);
 
 
-  // Array declaration. Use your macro to print the size of this.
-  int x[5];
-
-  // You can just use your macro here instead: PRINT_SIZE("student", you);
-  printf("size of %s : %zu bytes \n", "student", sizeof(you));
-
-  return 0;
+    return 0;
 }
+
